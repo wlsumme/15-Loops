1873fbe9e3f1c07ce1d6e8245735e53f4d2c24c5
William Summe <wlsumme@gmail.com>
2024-05-02 21:13:38 -0500

    Lops15

---
+++ 15Loops/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace _15Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i");
+            }
+        }               
+    }
+}
